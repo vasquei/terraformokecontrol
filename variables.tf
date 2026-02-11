8e7386a933eac00aded7f560d330c74a8737a5a3
vasquei <ivan.chinome@gmail.com>
2026-02-10 19:04:28 -0500

    3 commit

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "tenancy_ocid" { type = string }
 variable "user_ocid" { type = string }
 variable "fingerprint" { type = string }
-variable "private_key_path" { type = string }
+variable "private_key" { type = string }
 variable "region" { type = string }
 
 variable "compartment_ocid" { type = string }
